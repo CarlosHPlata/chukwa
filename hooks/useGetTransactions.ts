45147c161ae8c36180bbc3d463ea3c8439f34174
Carlos Plata <carlos.plata@rappi.com>
2025-06-11 16:01:03 +0200

    GH-4 Deleting old static repositories

--- hooks/useGetTransactions.ts
+++
@@ -1,8 +0,0 @@
-import { Transaction } from "@/domain/entities/Transaction";
-import { getTransactions } from "@/repositories/TransactionRepository";
-import { AsyncResponse } from "./AsyncResponse";
-import useAsyncValues from "./useAsyncValues";
-
-export default function useGetTransaction(): AsyncResponse<Transaction[]> {
-  return useAsyncValues(getTransactions);
-}
