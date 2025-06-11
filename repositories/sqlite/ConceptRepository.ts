679b0b53ef67ecd885f4f5345082515432d8d98c
Carlos Plata <carlos.ksa21@gmail.com>
2025-06-11 19:19:24 +0200

    GH-2 Removign builder pattern library

--- repositories/sqlite/ConceptRepository.ts
+++ repositories/sqlite/ConceptRepository.ts
@@ -1,9 +1,7 @@
+import * as schema from "@/db/schema";
 import { GetConcepts } from "@/domain/repositories/conceptRepository";
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import { SQLiteDatabase } from "expo-sqlite";
-import * as schema from "@/db/schema";
-import { Builder } from "builder-pattern";
-import { Concept } from "@/domain/entities/Concept";
 
 type GetConceptsFactory = (db: SQLiteDatabase) => GetConcepts;
 export const getConcepts: GetConceptsFactory = (db) => async () => {
@@ -16,11 +14,9 @@ export const getConcepts: GetConceptsFactory = (db) => async () => {
     return [];
   }
 
-  return conceptsDb.map((conceptDb) =>
-    Builder<Concept>({
-      id: conceptDb.id.toString(),
-      name: conceptDb.name,
-      icon: conceptDb.icon,
-    }).build(),
-  );
+  return conceptsDb.map((conceptDb) => ({
+    id: conceptDb.id.toString(),
+    name: conceptDb.name,
+    icon: conceptDb.icon,
+  }));
 };
